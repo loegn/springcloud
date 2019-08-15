6ca6b9de69abd4ab48bc019896c0b76e5c9be154
liangenmao <liangenmao@yuantutech.com>
2019-08-15 15:32:01 +0800

    add hystrix

--- custom/src/main/java/com/example/springcloud/custom/controller/HelloController.java
+++ custom/src/main/java/com/example/springcloud/custom/controller/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.springcloud.custom.controller;
 
-import com.example.springcloud.custom.sevice.HelloService;
+import com.example.springcloud.custom.sevice.HelloRemoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @Autowired
-    HelloService helloService;
+    HelloRemoteService helloRemoteService;
 
     @GetMapping(value = "/hi")
     public String hi(@RequestParam String name) {
-        String get = helloService.get(name);
-        String post = helloService.post(name);
+        String get = helloRemoteService.get(name);
+        String post = helloRemoteService.post(name);
         return get + "</br>" + post;
     }
 }
--- custom/src/main/java/com/example/springcloud/custom/sevice/HelloRemoteService.java
+++ custom/src/main/java/com/example/springcloud/custom/sevice/HelloRemoteService.java
@@ -1,5 +1,6 @@
 package com.example.springcloud.custom.sevice;
 
+import com.example.springcloud.custom.sevice.impl.HelloRemoteHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @date : 2019/08/15
  * @author: liangenmao
  */
-@FeignClient("provider-hi")
-public interface HelloService {
+@FeignClient(value = "provider-hi", fallback = HelloRemoteHystrix.class)
+public interface HelloRemoteService {
 
     @GetMapping("/getRequest")
     String get(@RequestParam String name);
