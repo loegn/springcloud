cf153999f1238f6d30b3443002b698a558047c8c
liangenmao <liangenmao@yuantutech.com>
2019-08-16 10:45:18 +0800

    add config center

--- custom/src/main/java/com/example/springcloud/custom/controller/HelloController.java
+++ custom/src/main/java/com/example/springcloud/custom/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springcloud.custom.controller;
 
 import com.example.springcloud.custom.sevice.HelloRemoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @Autowired
     HelloRemoteService helloRemoteService;
+    @Value("${neo.hello}")
+    private String hello;
 
     @GetMapping(value = "/hi")
     public String hi(@RequestParam String name) {
