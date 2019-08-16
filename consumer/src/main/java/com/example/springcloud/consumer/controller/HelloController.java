0268f390762f3155e8857642dc019474cc562ed1
liangenmao <liangenmao@yuantutech.com>
2019-08-16 17:42:39 +0800

    add bus

--- consumer/src/main/java/com/example/springcloud/consumer/controller/HelloController.java
+++ consumer/src/main/java/com/example/springcloud/consumer/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.example.springcloud.consumer.service.ApiRemoteService;
 import com.example.springcloud.consumer.service.HelloRemoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: liangenmao
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Autowired
     private HelloRemoteService helloRemoteService;
